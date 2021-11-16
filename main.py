b5053633aeb7c287a3a1f10c5115a42ad4daacfe
TonkoshkuraMisha <tonkoshkura.misha@gmail.com>
2021-11-16 17:02:51 +0200

    green screen

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import pygame
 
+FRAME_COLOR = (155, 188, 15)
+size = [600, 800]
+
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption('Snake')
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    screen.fill(FRAME_COLOR)
+    pygame.display.flip()
